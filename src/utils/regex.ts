79c94797fe960208cb7583576b8e3c1529ad4363
Jahidul Islam <jahidul.app@gmail.com>
2025-08-17 16:26:48 +0600

    feat: update regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,3 @@
 // export const phoneRegex = new RegExp(/^01[3-9]\d{8}$/);
-export const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+export const phoneRegex = /^\+?[0-9]\d{1,14}$/;
 export const timeRegex = new RegExp(/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/);
